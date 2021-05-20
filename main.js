0dc5364e5278a98bbd647a077712f573d72946ec
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2021-05-20 18:59:25 -0500

    nice

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const init = () => {
     document.getElementById('userInputSubmit').addEventListener('click', (e) => {
         let userText = document.getElementById('userInput').value;
         // console.log(userText);
-        document.getElementById('userInputConverted').innerHTML = userText;
+        let userTextSquished = squishify(userText);
+        document.getElementById('userInputConverted').innerHTML = userTextSquished;
     });
 };
 init();
+
+const squishify = (text) => {
+    return text;
+};
